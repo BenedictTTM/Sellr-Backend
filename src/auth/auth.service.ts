d3d31806d60b5beea495193b5625ce70b7eaaaa9
Benedict <palomakut1@gmail.com>
2025-06-13 21:16:40 +0000

    setup the prisma client

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
 //import {User , Bookmark} from "@prisma/client";
 
+export interface User {
+  id: number;
+  createdAt: Date;
+  email: string;
+  firstName: string;
+  lastName: string;
+  isPremium: boolean;
+}
 @Injectable()
 export class AuthService{
-     login() {
-    return 'I am a login endpoint';
-  }
-  signup() {
+     constructor(private prismaService: PrismaService) {}
+
+    signup() {
     return 'I am a signup endpoint';
   }
+  login() {
+    return 'I am a login endpoint';
+  }
+
 }
