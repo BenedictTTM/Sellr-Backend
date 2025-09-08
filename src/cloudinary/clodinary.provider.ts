81aa6e26b5c1de5440c35c456898d62dd12679c1
Benedict <palomakut1@gmail.com>
2025-09-08 09:01:43 +0000

    Fix/mearge error with main branch (cloudinary provider)

--- src/cloudinary/clodinary.provider.ts
+++ src/cloudinary/clodinary.provider.ts
@@ -1,11 +1,23 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CLOUDINARY } from './constants';
 
+export const CloudinaryProvider = {
+  provide: CLOUDINARY,
+  useFactory: () => {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    return cloudinary;
+  },
+};
 
 
-// CLOUDINARY_CLOUD_NAME=dsri
-// CLOUDINARY_API_KEY=71581
-// CLOUDINARY_API_SECRET=-KbZfjHulej
+// CLOUDINARY_CLOUD_NAME=dsriwu6yn
+// CLOUDINARY_API_KEY=715814567355674
+// CLOUDINARY_API_SECRET=-KbZfjHulejXEtPBMCATurHUSGw
 
-///Update the cloudinary api keys 
+
+
 
